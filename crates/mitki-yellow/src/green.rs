aa65da228776582833d90d503c6cbff0191a576b
gvozdvmozgu <gvozdvmozgu@gmail.com>
2024-10-02 18:20:44 -0700

    implement text trimming for `GreenToken` and refactor `GreenNode` to store `text_len`

--- crates/mitki-yellow/src/green.rs
+++ crates/mitki-yellow/src/green.rs
@@ -1,22 +1,61 @@
+use salsa::Database;
 use text_size::TextSize;
 use triomphe::ThinArc;
 
-use crate::{NodeOrToken, Symbol, SyntaxKind};
+use crate::{NodeOrToken, SyntaxKind};
 
 pub type Green<'db> = NodeOrToken<GreenNode<'db>, GreenToken<'db>>;
 
-#[salsa::interned]
+impl Green<'_> {
+    pub fn text_len(&self, db: &dyn Database) -> TextSize {
+        match self {
+            NodeOrToken::Node(node) => node.text_len(db),
+            NodeOrToken::Token(token) => TextSize::new(token.text(db).len() as u32),
+        }
+    }
+}
+
+#[salsa::interned(constructor = alloc)]
 pub struct GreenNode<'db> {
     pub kind: SyntaxKind,
+    #[return_ref]
     pub children: Vec<Green<'db>>,
+    pub text_len: TextSize,
+}
+
+impl<'db> GreenNode<'db> {
+    pub fn new(db: &'db dyn Database, kind: SyntaxKind, children: Vec<Green<'db>>) -> Self {
+        let text_len = children.iter().map(|child| child.text_len(db)).sum();
+        Self::alloc(db, kind, children, text_len)
+    }
 }
 
 #[salsa::interned]
 pub struct GreenToken<'db> {
     pub leading: GreenTrivia,
     pub kind: SyntaxKind,
-    pub text: Symbol<'db>,
+    #[return_ref]
+    pub text: Box<str>,
     pub trailing: GreenTrivia,
+}
+
+impl<'db> GreenToken<'db> {
+    fn leading_trailing_total_len(self, db: &'db dyn Database) -> (TextSize, TextSize, TextSize) {
+        let leading_len = self.leading(db).len();
+        let trailing_len = self.trailing(db).len();
+        let total_len = self.text(db).len() as u32;
+
+        (leading_len, trailing_len, total_len.into())
+    }
+
+    pub fn text_trimmed(self, db: &'db dyn Database) -> &'db str {
+        let (leading_len, trailing_len, total_len) = self.leading_trailing_total_len(db);
+
+        let start: usize = leading_len.into();
+        let end: usize = (total_len - trailing_len).into();
+
+        &self.text(db)[start..end]
+    }
 }
 
 #[derive(Clone, Eq, Hash, PartialEq)]
@@ -74,4 +113,31 @@ impl TriviaPiece {
 pub enum TriviaPieceKind {
     Whitespace,
     SingleLineComment,
+}
+
+#[cfg(test)]
+mod tests {
+    use salsa::DatabaseImpl;
+
+    use super::*;
+
+    fn whitespace(len: u32) -> GreenTrivia {
+        GreenTrivia::new(&[TriviaPiece::new(TriviaPieceKind::Whitespace, len.into())])
+    }
+
+    #[test]
+    fn token_text() {
+        let db = DatabaseImpl::new();
+
+        let token = GreenToken::new(
+            &db,
+            whitespace(3),
+            SyntaxKind::VAL_KW,
+            "\n\t val \t\t".into(),
+            whitespace(3),
+        );
+
+        assert_eq!("\n\t val \t\t", token.text(&db).as_ref());
+        assert_eq!("val", token.text_trimmed(&db));
+    }
 }
--- crates/mitki-yellow/src/syntax_kind.rs
+++ crates/mitki-yellow/src/syntax_kind.rs
@@ -1,5 +1,7 @@
 #[allow(non_camel_case_types)]
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 pub enum SyntaxKind {
+    VAL_KW,
+
     ERROR,
 }
