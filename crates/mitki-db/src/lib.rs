cbd1084e3f7275fd45c67ffea011b65c1228e282
gvozdvmozgu <gvozdvmozgu@gmail.com>
2024-10-07 07:16:22 -0700

    initial HIR layer

--- crates/mitki-db/src/lib.rs
+++ crates/mitki-db/src/lib.rs
@@ -1,5 +1,7 @@
 use camino::Utf8PathBuf;
 pub use mitki_errors::Diagnostic;
+use mitki_hir::Body;
+use mitki_yellow::GreenNode;
 use salsa::Database;
 
 #[salsa::input]
@@ -11,6 +13,20 @@ pub struct File {
 }
 
 #[salsa::tracked]
+impl File {
+    #[salsa::tracked]
+    fn parse(self, db: &dyn Database) -> GreenNode<'_> {
+        mitki_parse::module(db, self.text(db))
+    }
+
+    #[salsa::tracked(no_eq)]
+    fn hir(self, db: &dyn Database) -> Body {
+        let _module = self.parse(db);
+        Body::default()
+    }
+}
+
+#[salsa::tracked]
 pub fn check_file(db: &dyn Database, file: File) {
-    let _module = mitki_parse::module(db, file.text(db));
+    _ = file.hir(db);
 }
--- crates/mitki-yellow/src/lib.rs
+++ crates/mitki-yellow/src/lib.rs
@@ -1,11 +1,10 @@
+pub mod ast;
 mod builder;
 mod green;
-mod symbol;
 mod syntax_kind;
 
 pub use builder::Builder;
 pub use green::{Green, GreenNode, GreenToken, GreenTrivia, TriviaPiece, TriviaPieceKind};
-pub use symbol::Symbol;
 pub use syntax_kind::SyntaxKind;
 
 #[derive(Debug, Clone, Copy, Eq, Hash, PartialEq)]
