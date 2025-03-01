12d55aa8cabb3d2c4da374684bc0507c0ca9d146
Andrey Nikolaev <gvozdvmozgu@gmail.com>
2025-03-01 00:26:46 -0800

    do not store `SyntaxNodePtr` twice (#35)

--- crates/mitki-analysis/src/ast_map.rs
+++ crates/mitki-analysis/src/ast_map.rs
@@ -1,10 +1,10 @@
+use hashbrown::HashTable;
 use mitki_inputs::File;
 use mitki_parse::FileParse;
 use mitki_yellow::{RedNode, RedNodePtr, SyntaxKind};
-use rustc_hash::FxHashMap;
 use salsa::Database;
 
-use crate::arena::{Arena, Idx};
+use crate::arena::{Arena, Key};
 
 pub(crate) trait HasAstMap {
     fn ast_map(self, db: &dyn Database) -> &AstMap;
@@ -18,35 +18,64 @@ impl HasAstMap for File {
     }
 }
 
-#[derive(Debug, salsa::Update)]
+#[derive(Debug)]
 pub(crate) struct AstMap {
     arena: Arena<RedNodePtr>,
-    map: FxHashMap<RedNodePtr, Idx<RedNodePtr>>,
+    map: HashTable<Key<RedNodePtr>>,
+}
+
+unsafe impl salsa::Update for AstMap {
+    unsafe fn maybe_update(old_pointer: *mut Self, new_value: Self) -> bool {
+        let old = unsafe { &mut *old_pointer };
+
+        if old.arena.len() != new_value.arena.len() {
+            old.arena.clear();
+            old.map.clear();
+
+            old.arena.extend(new_value.arena);
+            build_hash_table(&old.arena, &mut old.map);
+
+            return true;
+        }
+
+        unsafe { salsa::Update::maybe_update(&mut old.arena, new_value.arena) }
+    }
 }
 
 impl AstMap {
     pub(crate) fn from_root(db: &dyn Database, root: &RedNode<'_>) -> Self {
         let mut arena = Arena::new();
+        let mut map = HashTable::default();
 
         root.children(db).for_each(|node| {
             if let SyntaxKind::FN = node.kind(db) {
                 arena.alloc(RedNodePtr::new(db, &node));
             }
         });
-
-        let mut map = FxHashMap::with_capacity_and_hasher(arena.len(), Default::default());
-        for (index, node) in arena.iter() {
-            map.insert(*node, index);
-        }
+        build_hash_table(&arena, &mut map);
 
         Self { arena, map }
     }
 
-    pub(crate) fn find_id(&self, db: &dyn Database, node: &RedNode<'_>) -> Idx<RedNodePtr> {
-        self.map[&RedNodePtr::new(db, node)]
+    pub(crate) fn find_id(&self, db: &dyn Database, node: &RedNode<'_>) -> Key<RedNodePtr> {
+        let ptr = RedNodePtr::new(db, node);
+        *self.map.find(hash_one(&ptr), |&key| self.arena[key] == ptr).unwrap()
     }
 
-    pub(crate) fn find_node(&self, index: Idx<RedNodePtr>) -> &RedNodePtr {
+    pub(crate) fn find_node(&self, index: Key<RedNodePtr>) -> &RedNodePtr {
         &self.arena[index]
     }
+}
+
+fn build_hash_table(arena: &Arena<RedNodePtr>, map: &mut HashTable<Key<RedNodePtr>>) {
+    for (key, value) in arena.iter_enumerated() {
+        let hash = hash_one(&value);
+        map.insert_unique(hash, key, |&key| hash_one(&arena[key]));
+    }
+}
+
+fn hash_one<T: std::hash::Hash>(t: &T) -> u64 {
+    use std::hash::BuildHasher as _;
+
+    std::hash::BuildHasherDefault::<rustc_hash::FxHasher>::default().hash_one(t)
 }
